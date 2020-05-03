d9b0c7945fee1dc3df1cb60ebfee2a6abb30d4a9
charliespy <charlie.spy@hotmail.com>
2020-05-03 14:35:59 +0000

    controls

--- new_view_controller.js
+++ new_view_controller.js
@@ -3,22 +3,23 @@ var index = 0;
 
 $( document ).ready(function() {
     for(var i = 0; i < places_list.length; i++) {
-      $("#menu_bar").append('<a class="dropdown-item" onclick="goToNextSchoolWithIndex(' + i + ')">' + places_list[i].title + '</a>');
+      $("#menu_bar").append('<a class="dropdown-item" onclick="goToNextPlaceWithIndex(' + i + ')">' + places_list[i].title + '</a>');
     }
 
     $("#title").text(places_list[index].title);
     $("#description1").text(places_list[index].description1);
     $("#description2").text(places_list[index].description2);
+    $("#heading").text('Virtual Tour Webb | ' + places_list[index].title);
     // $("#description2").attr("src", places_list[0].img);
     var panorama = new google.maps.StreetViewPanorama(
-            document.getElementById('pano'), {
-                position: {lat: places_list[index].lat, lng: places_list[index].lng},
-                pov: {
-                heading: places_list[index].heading,
-                pitch: places_list[index].pitch,
-                }
-            });
-          map.setStreetView(panorama);
+      document.getElementById('pano'), {
+        position: {lat: places_list[index].lat, lng: places_list[index].lng},
+        pov: {
+          heading: places_list[index].heading,
+          pitch: places_list[index].pitch,
+        }
+      });
+    map.setStreetView(panorama);
 });
 
 function goToNextPlace() {
@@ -27,44 +28,48 @@ function goToNextPlace() {
     $("#title").text(places_list[index].title);
     $("#description1").text(places_list[index].description1);
     $("#description2").text(places_list[index].description2);
+    $("#heading").text('Virtual Tour Webb | ' + places_list[index].title);
     var panorama = new google.maps.StreetViewPanorama(
-            document.getElementById('pano'), {
-                position: {lat: places_list[index].lat, lng: places_list[index].lng},
-                pov: {
-                heading: places_list[index].heading,
-                pitch: places_list[index].pitch,
-                }
-            });
-          map.setStreetView(panorama);
-  }  
+      document.getElementById('pano'), {
+        position: {lat: places_list[index].lat, lng: places_list[index].lng},
+        pov: {
+          heading: places_list[index].heading,
+          pitch: places_list[index].pitch,
+        }
+      });
+    map.setStreetView(panorama);
+  } else {
+    window.location.href = "index.html";
+  }
 }
 
 function initialize() {
   var panorama = new google.maps.StreetViewPanorama(
-            document.getElementById('pano'), {
-                position: {lat: places_list[index].lat, lng: places_list[index].lng},
-                pov: {
-                heading: places_list[index].heading,
-                pitch: places_list[index].pitch,
-                }
-            });
-          map.setStreetView(panorama);
+    document.getElementById('pano'), {
+      position: {lat: places_list[index].lat, lng: places_list[index].lng},
+        pov: {
+          heading: places_list[index].heading,
+          pitch: places_list[index].pitch,
+        }
+    });
+  map.setStreetView(panorama);
 }
 
-function goToNextSchoolWithIndex(gotoIndex) {
+function goToNextPlaceWithIndex(gotoIndex) {
   if (index < places_list.length) {
     index = gotoIndex;
     $("#title").text(places_list[index].title);
     $("#description1").text(places_list[index].description1);
     $("#description2").text(places_list[index].description2);
+    $("#heading").text('Virtual Tour Webb | ' + places_list[index].title);
     var panorama = new google.maps.StreetViewPanorama(
-            document.getElementById('pano'), {
-                position: {lat: places_list[index].lat, lng: places_list[index].lng},
-                pov: {
-                heading: places_list[index].heading,
-                pitch: places_list[index].pitch,
-                }
-            });
+      document.getElementById('pano'), {
+        position: {lat: places_list[index].lat, lng: places_list[index].lng},
+        pov: {
+          heading: places_list[index].heading,
+          pitch: places_list[index].pitch,
+        }
+      });
     map.setStreetView(panorama);
   }  
 }
