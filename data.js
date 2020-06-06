9d59a3d24c2b767ee4d2c017385f189321d856f5
Sophadeth Rithya <sophadethrithya@gmail.com>
2020-06-06 21:11:28 +0000

    Modified data

---
+++ data.js
@@ -0,0 +1,578 @@
+var data = {
+  "school_name": "Cal Poly Pomona",
+  "address": "3801 W Temple Ave, Pomona, CA 91768, United States",
+  "location": {
+    "lat": 34.0588862,
+    "lng": -117.8193781
+  },
+  "buildings": [
+    {
+      "title": "Cal Poly Pomona",
+      "description1": "This is the Cal Poly Pomona.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0588862,
+      "lng": -117.8193781,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Student Services Building",
+      "description1": "This is the Student Services Building.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0587707,
+      "lng": -117.8190623,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Cal Poly Pomona Parking Lot C",
+      "description1": "This is the Cal Poly Pomona Parking Lot C.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0585063,
+      "lng": -117.8191273,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "PolyTransfer Office",
+      "description1": "This is the PolyTransfer Office.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0584103,
+      "lng": -117.8189416,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Student Services Building Building 121",
+      "description1": "This is the Student Services Building Building 121.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0581837,
+      "lng": -117.819306,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "CLA Bldg 98",
+      "description1": "This is the CLA Bldg 98.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0597926,
+      "lng": -117.8194287,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Cal Poly Pomana Pre-College TRIO Programs Building 13D",
+      "description1": "This is the Cal Poly Pomana Pre-College TRIO Programs Building 13D.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0584891,
+      "lng": -117.820453,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Voorhis Rock",
+      "description1": "This is the Voorhis Rock.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.05985150000001,
+      "lng": -117.8187655,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Bldg. 98 - Classroom/Laboratory/Administration (C/L/A)",
+      "description1": "This is the Bldg. 98 - Classroom/Laboratory/Administration (C/L/A).",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0599984,
+      "lng": -117.8194736,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Cal Poly Pomona Campus Tours",
+      "description1": "This is the Cal Poly Pomona Campus Tours.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0598714,
+      "lng": -117.8200423,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "CPP Test Center",
+      "description1": "This is the CPP Test Center.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0599487,
+      "lng": -117.8199554,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Art Department and Engineering Annex Building 13",
+      "description1": "This is the Art Department and Engineering Annex Building 13.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.05873609999999,
+      "lng": -117.8208096,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Cal Poly Pomona Army ROTC",
+      "description1": "This is the Cal Poly Pomona Army ROTC.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0583524,
+      "lng": -117.8206737,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Voorhis Park",
+      "description1": "This is the Voorhis Park.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.05997849999999,
+      "lng": -117.8185038,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Cal Poly Pomona Distribution Services - Receiving",
+      "description1": "This is the Cal Poly Pomona Distribution Services - Receiving.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0603174,
+      "lng": -117.8193125,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "ARB Building",
+      "description1": "This is the ARB Building.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.05836960000001,
+      "lng": -117.817706,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "School",
+      "description1": "This is the School.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0576744,
+      "lng": -117.8206145,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Cal poly",
+      "description1": "This is the Cal poly.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0576744,
+      "lng": -117.8206145,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Engineering Meadow",
+      "description1": "This is the Engineering Meadow.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0592031,
+      "lng": -117.8213431,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Computer Lab At Bldg 15",
+      "description1": "This is the Computer Lab At Bldg 15.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0580872,
+      "lng": -117.8211396,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Presidents Office",
+      "description1": "This is the Presidents Office.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0576706,
+      "lng": -117.8208081,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Administration Building",
+      "description1": "This is the Administration Building.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0576706,
+      "lng": -117.8208081,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Horseshoe Hill",
+      "description1": "This is the Horseshoe Hill.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.05714119999999,
+      "lng": -117.8196029,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Rose Garden",
+      "description1": "This is the Rose Garden.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.060493,
+      "lng": -117.8203356,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Cal Poly Pomona Parking Lot F8",
+      "description1": "This is the Cal Poly Pomona Parking Lot F8.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0589449,
+      "lng": -117.8171578,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Cal Poly Pomona College of Engineering Bldg 17",
+      "description1": "This is the Cal Poly Pomona College of Engineering Bldg 17.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0600229,
+      "lng": -117.8211545,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "University Plaza Bldg 26",
+      "description1": "This is the University Plaza Bldg 26.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.057053,
+      "lng": -117.8200433,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Cal Poly Pomona University Library",
+      "description1": "This is the Cal Poly Pomona University Library.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.05791720000001,
+      "lng": -117.8214495,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Engineering Bldg 9",
+      "description1": "This is the Engineering Bldg 9.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.05885740000001,
+      "lng": -117.8222014,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Centerpointe Dining Commons Bldg. 72",
+      "description1": "This is the Centerpointe Dining Commons Bldg. 72.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0566361,
+      "lng": -117.8184635,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "CBA Bldg 162",
+      "description1": "This is the CBA Bldg 162.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0613114,
+      "lng": -117.8195386,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "CBA Faculty Lecture Hall",
+      "description1": "This is the CBA Faculty Lecture Hall.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0613125,
+      "lng": -117.8195625,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "College of Engineering Building 9",
+      "description1": "This is the College of Engineering Building 9.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0591543,
+      "lng": -117.8222974,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Lollicup",
+      "description1": "This is the Lollicup.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0566347,
+      "lng": -117.8181778,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Magnolia Ln Parking",
+      "description1": "This is the Magnolia Ln Parking.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0604682,
+      "lng": -117.8170379,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Parking Structure 1",
+      "description1": "This is the Parking Structure 1.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0602864,
+      "lng": -117.8168535,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Cal Poly Pomona College of Business Administration",
+      "description1": "This is the Cal Poly Pomona College of Business Administration.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0613685,
+      "lng": -117.819886,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Student Affairs Information Technology Services Bldg 91",
+      "description1": "This is the Student Affairs Information Technology Services Bldg 91.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0613492,
+      "lng": -117.8186412,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Cal Poly",
+      "description1": "This is the Cal Poly.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.06050190000001,
+      "lng": -117.8169814,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "CBA Bldg 163",
+      "description1": "This is the CBA Bldg 163.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0613179,
+      "lng": -117.8204233,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Parking Lot F4",
+      "description1": "This is the Parking Lot F4.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0608755,
+      "lng": -117.817347,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Sicomoro Residence Hall Bldg. 73",
+      "description1": "This is the Sicomoro Residence Hall Bldg. 73.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0563597,
+      "lng": -117.8184474,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Cal Poly Pomona Foundation Inc",
+      "description1": "This is the Cal Poly Pomona Foundation Inc.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.05627189999999,
+      "lng": -117.8198468,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "CBA Bldg 164",
+      "description1": "This is the CBA Bldg 164.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0615675,
+      "lng": -117.8196546,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Cal Poly Pomona Building 20",
+      "description1": "This is the Cal Poly Pomona Building 20.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.06143000000001,
+      "lng": -117.820624,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "cal poly uni",
+      "description1": "This is the cal poly uni.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0579787,
+      "lng": -117.8225193,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Cal Poly Federal Credit Union",
+      "description1": "This is the Cal Poly Federal Credit Union.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0565936,
+      "lng": -117.8213139,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Child Care Center Building 116",
+      "description1": "This is the Child Care Center Building 116.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0560873,
+      "lng": -117.8194685,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "College of Education and Integrative Studies Building 6",
+      "description1": "This is the College of Education and Integrative Studies Building 6.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0585891,
+      "lng": -117.822838,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "University Park",
+      "description1": "This is the University Park.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0565305,
+      "lng": -117.8213945,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Palmitas Residence Hall",
+      "description1": "This is the Palmitas Residence Hall.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.06047119999999,
+      "lng": -117.8223214,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Sandra Office",
+      "description1": "This is the Sandra Office.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0593457,
+      "lng": -117.8228954,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Cal Poly Pomona University Housing Services",
+      "description1": "This is the Cal Poly Pomona University Housing Services.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0609847,
+      "lng": -117.821935,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Cal Poly DORM",
+      "description1": "This is the Cal Poly DORM.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0609336,
+      "lng": -117.8166704,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Cedritos Residence Hall",
+      "description1": "This is the Cedritos Residence Hall.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.06152429999999,
+      "lng": -117.8212098,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Bronco Bookstore",
+      "description1": "This is the Bronco Bookstore.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0559688,
+      "lng": -117.8204743,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Bank of America ATM",
+      "description1": "This is the Bank of America ATM.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0563233,
+      "lng": -117.8214406,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Secoya Residence Hall Bldg. 74",
+      "description1": "This is the Secoya Residence Hall Bldg. 74.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0559227,
+      "lng": -117.8182972,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "Bronco Student Center",
+      "description1": "This is the Bronco Student Center.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.05619,
+      "lng": -117.8212629,
+      "heading": 34,
+      "pitch": 10
+    },
+    {
+      "title": "University Housing Services",
+      "description1": "This is the University Housing Services.",
+      "description2": "Your next stop is the next building.",
+      "lat": 34.0559003,
+      "lng": -117.8182168,
+      "heading": 34,
+      "pitch": 10
+    }
+  ]
+}
+$( document ).ready(function() {
+function httpGet(theUrl)
+{
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+  xmlHttp.send( null );
+  return xmlHttp.responseText;
+}
+
+var data2 = httpGet("https://orderlyrepentantspecialist--sophadethrithya.repl.co/getBuildings")
+}
+
+
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+
+$.get(
+    "somepage.php",
+    {paramOne : 1, paramX : 'abc'},
+    function(data) {
+       alert('page content: ' + data);
+    }
+);
