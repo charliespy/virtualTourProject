b500dd1d294fe01552c0854c5f96f8cc2c556454
charliesun01 <42754940+charliespy@users.noreply.github.com>
2021-07-19 15:30:57 +0000

    change descriptions

--- new_view_controller.js
+++ new_view_controller.js
@@ -45,6 +45,29 @@ function goToNextPlace() {
   }
 }
 
+function goToLastPlace() {
+  if (index!=0){
+    index--;
+  }
+  if (index < places_list.length) {
+    $("#title").text(places_list[index].title);
+    $("#description1").text(places_list[index].description1);
+    $("#description2").text(places_list[index].description2);
+    $("#heading").text('Virtual Tour Webb | ' + places_list[index].title);
+    var panorama = new google.maps.StreetViewPanorama(
+      document.getElementById('pano'), {
+        position: {lat: places_list[index].lat, lng: places_list[index].lng},
+        pov: {
+          heading: places_list[index].heading,
+          pitch: places_list[index].pitch,
+        }
+      });
+    map.setStreetView(panorama);
+  } else {
+    window.location.href = "index.html";
+  }
+}
+
 function initialize() {
   var panorama = new google.maps.StreetViewPanorama(
     document.getElementById('pano'), {
