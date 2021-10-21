a8d2cf96bd73ed94f12333142a49637d0764a710
charliespy <42754940+charliespy@users.noreply.github.com>
2021-10-21 11:31:34 -0700

    Add: places from last shoot

--- new_view_controller.js
+++ new_view_controller.js
@@ -8,52 +8,32 @@ var index = 0;
 
 function goToNextPlace() {
   index++;
-  if (index < places_list.length) {
-    $("#title").text(places_list[index].title);
-    $("#description1").text(places_list[index].description1);
-    $("#description2").text(places_list[index].description2);
-    $("#heading").text('Virtual Tour Webb | ' + places_list[index].title);
-    const location = { lat: places_list[index].lat, lng: places_list[index].lng };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      center: location,
-      zoom: 14,
-    });
-    const panorama = new google.maps.StreetViewPanorama(
-      document.getElementById("pano"),
-      {
-        position: { lat: places_list[index].lat, lng: places_list[index].lng },
-        pov: {
-          heading: places_list[index].heading,
-          pitch: places_list[index].pitch,
-        },
-      }
-    );
-    map.setStreetView(panorama);
 
-    panorama.addListener("links_changed", () => {
-      console.log('The panoId now is ' + panorama.getPano());
-      index = findIndexByPano(panorama.getPano());
-      console.log('The index now is ' + index);
-  
-      $("#title").text(places_list[index].title);
-      $("#description1").text(places_list[index].description1);
-      $("#heading").text('Virtual Tour Webb | ' + places_list[index].title);
-  
-      if (places_list[index].links != undefined){
-        for (let i = 0; i < places_list[index].links.length; i++) {
-          panorama.getLinks().push({
-            description: places_list[index].links[i].description,
-            heading: places_list[index].links[i].heading,
-            pano: places_list[index].links[i].link,
-          });
-          // console.log(index);
-          // console.log(findIndexByPano(places_list[index].links[i].link));
-        }
-      }
-    });
-  } else {
-    window.location.href = "index.html";
-  }
+  $("#title").text(places_list[index].title);
+  $("#description1").text(places_list[index].description1);
+  $("#description2").text(places_list[index].description2);
+  $("#heading").text('Virtual Tour Webb | ' + places_list[index].title);
+
+  const location = { lat: places_list[index].lat, lng: places_list[index].lng };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: location,
+    zoom: 14,
+  });
+  const panorama = new google.maps.StreetViewPanorama(
+    document.getElementById("pano"),
+    {
+      position: { lat: places_list[index].lat, lng: places_list[index].lng },
+      pov: {
+        heading: places_list[index].heading,
+        pitch: places_list[index].pitch,
+      },
+    }
+  );
+  map.setStreetView(panorama);
+
+  console.log('The panoId now is ' + panorama.getPano());
+  index = findIndexByPano(panorama.getPano());
+  console.log('The index now is ' + index);
 }
 
 // function goToLastPlace() {
@@ -125,7 +105,7 @@ function myInitialize() {
     $("#description1").text(places_list[index].description1);
     $("#heading").text('Virtual Tour Webb | ' + places_list[index].title);
 
-    if (places_list[index].links != undefined){
+    if (places_list[index].links != undefined) {
       for (let i = 0; i < places_list[index].links.length; i++) {
         panorama.getLinks().push({
           description: places_list[index].links[i].description,
@@ -178,11 +158,11 @@ function goToNextPlaceWithIndex(gotoIndex) {
     //   console.log('The panoId now is ' + panorama.getPano());
     //   index = findIndexByPano(panorama.getPano());
     //   console.log('The index now is ' + index);
-  
+
     //   $("#title").text(places_list[index].title);
     //   $("#description1").text(places_list[index].description1);
     //   $("#heading").text('Virtual Tour Webb | ' + places_list[index].title);
-  
+
     //   if (places_list[index].links != undefined){
     //     for (let i = 0; i < places_list[index].links.length; i++) {
     //       panorama.getLinks().push({
